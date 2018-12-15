5fcbcbf14aa61f279310a2842a438f530204e8f8
Brian Dupuis <brian.dupuis@genesys.com>
2018-12-14 20:43:44 -0500

    Day 14 part 2 and some 2015 ones.

---
+++ src/app/2015/day4part2.elm
@@ -0,0 +1,70 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, text, textarea, pre)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import Dict exposing (Dict)
+import List exposing (..)
+import Array exposing (..)
+import Set exposing (..)
+import Debug exposing (..)
+import MD5 exposing (..)
+
+showDebug = True
+
+maybeDebug text a =
+    if showDebug then
+        Debug.log text a
+    else
+        a
+
+calculate : String -> String
+calculate inputVal =
+    calculatePossibleHash inputVal 0
+
+calculatePossibleHash : String -> Int -> String
+calculatePossibleHash seed current =
+    let 
+        hash =
+            MD5.hex (seed ++ (String.fromInt current))
+    
+    in
+        if String.startsWith "000000" hash then
+            String.fromInt current 
+        else
+            calculatePossibleHash seed (current + 1)
+
+type alias Model =
+    { result : String }
+
+initialModel : Model
+initialModel =
+    { result = "" }
+
+type Msg
+    = Calculate String
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Calculate newValue ->
+            { model | result = calculate newValue }
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ 
+          div [] [text <| model.result ]
+        , textarea [ onInput Calculate ] []
+        ]
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
+
---
+++ src/app/2018/day14part2.js
@@ -0,0 +1,32 @@
+const searchFor = "236021";
+
+let recipes = [3,7],
+    elves = [0,1],
+    done = false,
+    numRecipes = 2
+
+debugger;
+while (!done) {
+    if (numRecipes > 3000000) {
+        let string = recipes.join('');
+        console.log(string.indexOf(searchFor));
+        done = true;
+        continue;
+    }
+
+    let newRecipe = recipes[elves[0]] + recipes[elves[1]];
+    if (newRecipe >= 10) {
+        recipes.push(1);
+        newRecipe = newRecipe - 10;
+        numRecipes++;
+    }
+    recipes.push(newRecipe);
+
+    numRecipes++;
+
+    elves[0] = (elves[0] + recipes[elves[0]] + 1) % numRecipes;
+    elves[1] = (elves[1] + recipes[elves[1]] + 1) % numRecipes;
+}
+
+
+</script>
